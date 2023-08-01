589302ff3ff6413a831a200753a97f737c3eb803
Jose Lima <jalimag0206@gmail.com>
2023-08-01 02:07:54 -0300

    feat: implementing new techs

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -6,39 +6,59 @@ const skillLevel = document.querySelector('.level-skill');
 const mySkills = [
   {
     title: 'HTML5',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid reiciendis, veritatis beatae non velit sapiente voluptatem delectus fugit nostrum dolorem hic voluptas animi cum nam, nobis deserunt aut harum! Sit!',
+    content: 'O HTML é uma linguagem de marcação para desenvolvimento de aplicações web, possuo conhecimento avançado na linguagem utilizando de tags semânticas e acessibiliidade.',
     level: 'Nível: avançado'
   },
   {
     title: 'CSS',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid reiciendis, veritatis beatae non velit sapiente voluptatem delectus fugit nostrum dolorem hic voluptas animi cum nam, nobis deserunt aut harum! Sit!',
+    content: 'O CSS é uma linguagem de estilo mundialmente conhecida, possuo conhecimento em tópicos como animações,grid, flexbox, responsividade, animações e entre outros.',
     level: 'Nível: avançado'
   },
   {
     title: 'Javascript',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid reiciendis, veritatis beatae non velit sapiente voluptatem delectus fugit nostrum dolorem hic voluptas animi cum nam, nobis deserunt aut harum! Sit!',
+    content: 'O Javascript foi uma das primeiras linguagens de programação que comecei a estudar e hoje posso dizer que possuo conhecimento em diversos assuntos importantes como DOM,ES6,AJAX e entre outros.',
+    level: 'Nível: avançado'
+  },
+  {
+    title: 'Bootstrap',
+    content: 'O Bootstrap é uma biblioteca de css na qual nos facilita o desenvolvimento de páginas responsivas por meio de um conjunto de classes predefinidos pela biblioteca',
+    level: 'Nível: avançado'
+  },
+  {
+    title: 'Jquery',
+    content: 'lorem',
     level: 'Nível: avançado'
   },
   {
     title: 'Typescript',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid reiciendis, veritatis beatae non velit sapiente voluptatem delectus fugit nostrum dolorem hic voluptas animi cum nam, nobis deserunt aut harum! Sit!',
+    content: 'O typescript é um superset do javascript, fornecendo recursos como tipagem estática, interface permitindo a criação de um código mais escalável e de qualidade.',
     level: 'Nível: intermediário'
   },
   {
     title: 'React',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid reiciendis, veritatis beatae non velit sapiente voluptatem delectus fugit nostrum dolorem hic voluptas animi cum nam, nobis deserunt aut harum! Sit!',
-    level: 'Nível: avançado'
-  },
-  {
-    title: 'Redux',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid reiciendis, veritatis beatae non velit sapiente voluptatem delectus fugit nostrum dolorem hic voluptas animi cum nam, nobis deserunt aut harum! Sit!',
+    content: 'O React é uma biblioteca muito usada para construção de SPAs, possuo um ótimo conhecimento em react e procuro me aprimorar mais e mais a cada dia, caso tenha interesse tenho diversos projetos utilizando este biblioteca em meu github.',
     level: 'Nível: intermediário'
   },
   {
+    title: 'Redux',
+    content: 'O redux é uma biblioteca para gerenciamento do estado global de uma aplicação, com ela é possivel gerenciar os estados de um componente de maneira mais eficiente e escalável.',
+    level: 'Nível: básico/intermediário'
+  },
+  {
     title: 'Styled-components',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid reiciendis, veritatis beatae non velit sapiente voluptatem delectus fugit nostrum dolorem hic voluptas animi cum nam, nobis deserunt aut harum! Sit!',
+    content: 'é uma biblioteca para estilização de components, uma das suas principais características é o CSS-in-JS permitindo escrever códigos JavaScript juntamente de um código CSS, uma das minhas bibliotecas favoritas',
     level: 'Nível: avançado'
-  }
+  },
+  {
+    title: 'Git',
+    content: 'lorem',
+    level: 'Nível: avançado'
+  },
+  {
+    title: 'Github',
+    content: 'lorem',
+    level: 'Nível: avançado'
+  },
 ];
 
 const setSkill = (title, content, level) => {
@@ -48,7 +68,7 @@ const setSkill = (title, content, level) => {
 }
 
 const clearInfo = () => {
-  skillName.textContent = ''
+  skillName.textContent = '/* Passe o mouse ou clique em alguma tech para ver mais sobre ela */'
   skillContent.textContent = ''
   skillLevel.textContent = ''
 }
